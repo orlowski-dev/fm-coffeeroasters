462209344aa977affca4e418b5d0f62cd64949a4
orlowski-dev <orlowski.dev@gmail.com>
2024-01-10 15:27:02 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,77 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "dark-cyan": "#0e8784",
+        "dark-grey-blue": "#333D4B",
+        "pale-orange": "#FDD6BA",
+        "light-cream": "#FEFCF7",
+        grey: "#83888F",
+      },
+      fontFamily: {
+        barlow: "var(--barlow)",
+        fraunces: "var(--fraunces)",
+      },
+      fontSize: {
+        title: [
+          "9.375rem",
+          {
+            lineHeight: "4.5rem",
+            fontWeight: 900,
+          },
+        ],
+        h1: [
+          "4.5rem",
+          {
+            lineHeight: "4.5rem",
+            fontWeight: 700,
+          },
+        ],
+        h2: [
+          "2.5rem",
+          {
+            lineHeight: "3rem",
+            fontWeight: 700,
+          },
+        ],
+        h3: [
+          "2rem",
+          {
+            lineHeight: "2.25rem",
+            fontWeight: 700,
+          },
+        ],
+        h4: [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            fontWeight: 700,
+          },
+        ],
+        body: [
+          "1rem",
+          {
+            lineHeight: "1.625rem",
+          },
+        ],
+        "nav-menu": [
+          "0.75rem",
+          {
+            lineHeight: "0.9375rem",
+            fontWeight: 700,
+            letterSpacing: "0.0625rem",
+          },
+        ],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
